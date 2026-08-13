8061a5b9a2a90753636fc4e92e0073a6f4d023a7
rajneesh <rk5677283@gmail.com>
2026-08-14 04:41:22 +0530

    updated code

--- myproject5/blog/views.py
+++ myproject5/blog/views.py
@@ -7,9 +7,12 @@ def blog_details(request):
     post = {
         "title": "My First Blog Post",
         "content": "This is the content of my first blog post.",
-        "author": None,
+        "author": "yes",
         'comment_count': 4,
         "date_posted": datetime.now(),
+        "price": 19.99,
+        "number": 10,
+        'tags': ['django', 'python', 'web development'],
     }
     return render(request, 'blog/blog_details.html',{"post":post})
 # Create your views here.
